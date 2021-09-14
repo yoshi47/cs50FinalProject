8cf70bc369ad99df4a514c93bfb145cfc03982b3
yoshi47 <64204237+yoshi47@users.noreply.github.com>
2021-09-14 22:32:46 +0900

    update:金額の見た目

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 # 車以外の移動、検索結果の保存、ホテルの値段（最安値などの選択肢）、予算の詳細、FlaskFormを使う、csrfトークン、selectのリストを別ファイルに、ロード画面の変化
 
+locale.setlocale(locale.LC_NUMERIC, 'ja_JP')
 hotel = 8000
 
 
@@ -21,15 +22,17 @@ def utility_processor():
 
 @app.route('/', methods=["GET", "POST"])
 def home():  # put application's code here
+  global hotel
+
   if request.method == "POST":
     # 出発地
     start = request.form.get('start')
     # 到着地
     goal = request.form.get('goal')
     # 人数
-    people = request.form.get('people')
+    people = int(request.form.get('people'))
     # 日数
-    days = request.form.get('days')
+    days = int(request.form.get('days'))
 
     # 緯度・経度の取得
     startCoord = getPoint(start)
@@ -37,7 +40,10 @@ def home():  # put application's code here
 
     # 移動にかかる料金
     fare = carPrices(startCoord, goalCoord)
+
     # 宿代を足す
+    hotel *= days
+    hotel *= people
     fare += hotel
 
     info = {'start': start, 'goal': goal, 'people': people, 'days': days, 'result': fare}
